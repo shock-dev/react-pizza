878602c56080d6199e40063f0dbfdc272987f0fc
Andrey <shock.work@mail.ru>
2020-11-27 15:49:02 +0300

    realized categories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
 function Categories({ items }) {
+    const [activeItem, secActiveItem] = React.useState(null);
+
+    const onSelectItem = (index) => {
+        secActiveItem(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                {
-                    items.map(item => <li>{item}</li>)
+                <li
+                    onClick={() => onSelectItem(null)}
+                    className={activeItem === null ? 'active' : ''}
+                >
+                    Все
+                </li>
+                { items &&
+                    items.map((item, index) => (
+                        <li
+                            onClick={() => onSelectItem(index)}
+                            className={activeItem === index ? 'active' : ''}
+                            key={item + '-' + index}
+                        >
+                            {item}
+                        </li>
+                    ))
                 }
             </ul>
         </div>
