3261d0fbb134401d8b8b1bbc24c94a7425d6ebcd
Andrey <shock.work@mail.ru>
2020-12-16 00:01:44 +0300

    dynamic output of products

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import { Header } from './components';
 import { Home, Cart} from "./components/pages";
 import { Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
+    const [products, setProducts] = React.useState([])
+
+    React.useEffect(() => {
+        axios
+            .get('http://localhost:3000/pizzas')
+            .then(products => setProducts(products.data))
+    }, [])
+
     return (
         <div className="wrapper">
             <Header/>
 
             <div className="content">
-                <Route path='/' component={Home} exact/>
+                <Route path='/' render={() => <Home items={products}/>} exact/>
                 <Route path='/cart' component={Cart} exact/>
             </div>
         </div>
