eef94c8657204240eeaa7a924bdc9c3cf3fa0bfe
Andrey <shock.work@mail.ru>
2021-03-03 15:50:37 +0300

    types for pizza

--- src/types/pizza.ts
+++ src/types/pizza.ts
@@ -8,3 +8,25 @@ export interface Pizza {
     category: number,
     rating: number
 }
+
+export interface PizzaState {
+    pizzas: Pizza[];
+    isLoading: boolean;
+}
+
+export enum PizzaActionTypes {
+    SET_PIZZA = 'SET_PIZZA',
+    CHANGE_LOADING = 'CHANGE_LOADING'
+}
+
+interface SetPizzas {
+    type: PizzaActionTypes.SET_PIZZA;
+    payload: Pizza[];
+}
+
+interface ChangeLoading {
+    type: PizzaActionTypes.CHANGE_LOADING;
+    payload: boolean;
+}
+
+export type PizzaAction = SetPizzas | ChangeLoading;
