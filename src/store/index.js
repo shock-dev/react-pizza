655e9d06cbf4b2ef1a4602666f68ba5d6e679822
Andrey <shock.work@mail.ru>
2021-03-13 18:39:35 +0300

    delete redux logic

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import root from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(root, composeWithDevTools());
-
-export default store;
