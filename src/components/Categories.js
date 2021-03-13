7bfd5f2ab62d68bd90459939f81e3a4e20a9269b
Andrey <shock.work@mail.ru>
2021-03-13 17:11:53 +0300

    check items in Categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,27 +1,33 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 
 const Categories = ({ items }) => {
     const [categoryActive, setCategoryActive] = useState(null);
+
+    const onSelectItem = (index) => {
+        setCategoryActive(index);
+    }
 
     return (
         <div className="categories">
             <ul>
                 <li
                     className={classNames({'active': categoryActive === null})}
-                    onClick={setCategoryActive.bind(null, null)}
+                    onClick={onSelectItem.bind(null, null)}
                 >
                     Все
                 </li>
-                {items.map((category, index) =>
-                    <li
-                        key={index}
-                        className={classNames({'active': index === categoryActive})}
-                        onClick={setCategoryActive.bind(null, index)}
-                    >
-                        {category}
-                    </li>
-                )}
+                {items &&
+                    items.map((category, index) =>
+                        <li
+                            key={index}
+                            className={classNames({'active': index === categoryActive})}
+                            onClick={onSelectItem.bind(null, index)}
+                        >
+                            {category}
+                        </li>
+                    )
+                }
             </ul>
         </div>
     );
