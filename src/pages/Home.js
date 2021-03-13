971583b1b9be52e73dcb054966ce4423407f14e8
Andrey <shock.work@mail.ru>
2021-03-13 21:38:21 +0300

    use props and getting pizzas from App.js in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Categories, Sort, Card } from '../components';
 
-const Home = () => {
+const Home = ({ items }) => {
     const categories = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
     const sorts = [
         { name: 'популярности', type: 'popular', order: 'desc' },
         { name: 'цене', type: 'price', order: 'desc' },
         { name: 'алфавит', type: 'name', order: 'asc' },
     ];
-    const pizzas = [];
 
     return (
         <div className="content">
@@ -19,7 +18,7 @@ const Home = () => {
                 </div>
                 <h2 className="content__title">Все пиццы</h2>
                 <div className="content__items">
-                    {pizzas.map(pizza =>
+                    {items.map(pizza =>
                         <Card
                             key={pizza.id}
                             {...pizza}
