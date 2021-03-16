e31db15dffe09a9b3255a1533706b8448ebf312a
Andrey <shock.work@mail.ru>
2021-03-16 22:34:08 +0300

    change api url

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -9,7 +9,7 @@ export const fetchPizzas = (category, sortBy) => dispatch => {
     dispatch(setLoaded(false));
 
     axios
-        .get(`http://localhost:5000/pizzas?${
+        .get(`/pizzas?${
             category !== null ? `category=${ category }` : ''
         }&_sort=${ sortBy.type }&_order=${ sortBy.order }`)
         .then(({ data }) => {
