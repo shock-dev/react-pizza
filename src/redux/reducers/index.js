6394fdc17b9ebe99d67d1b22506cb5bef3ddbb1f
Andrey <shock.work@mail.ru>
2021-03-16 18:41:49 +0300

    import reducer into root

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
+import { combineReducers } from 'redux';
 import filters from './filters';
 import pizzas from './pizzas';
-import { combineReducers } from 'redux';
+import cart from './cart';
 
 const root = combineReducers({
     filters,
-    pizzas
+    pizzas,
+    cart
 });
 
 export default root;
