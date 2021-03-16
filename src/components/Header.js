6b3ab3655c76a3bfdc0e1962156ca59e9d7c0158
Andrey <shock.work@mail.ru>
2021-03-16 18:41:20 +0300

    outputting total price and count of pizzas in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import logoSvg from '../assets/img/pizza-logo.svg';
 import { Link } from 'react-router-dom';
 import { Button } from '.';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const { totalPrice, totalCount } = useSelector(({ cart }) => cart);
+
     return (
         <div className="header">
             <div className="container">
@@ -18,7 +21,7 @@ const Header = () => {
                 <div className="header__cart">
                     <Link to="/cart">
                         <Button className="button--cart">
-                            <span>15 ₽</span>
+                            <span>{totalPrice} ₽</span>
                             <div className="button__delimiter" />
                             <svg
                                 width="18"
@@ -48,7 +51,7 @@ const Header = () => {
                                     strokeLinejoin="round"
                                 />
                             </svg>
-                            <span>15</span>
+                            <span>{totalCount}</span>
                         </Button>
                     </Link>
                 </div>
