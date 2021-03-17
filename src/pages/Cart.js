3115a1856c4f2462dbe56722d8411ae1d2c9c74c
Andrey <shock.work@mail.ru>
2021-03-17 12:54:40 +0300

    change name of Cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CartComponent from '../components/Cart/CartComponent';
+import CartComponent from '../components/Cart/Cart';
 import EmptyCart from '../components/Cart/EmptyCart'
 
 const Cart = () => {
