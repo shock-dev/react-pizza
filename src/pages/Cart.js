97b6f25f25810879ff49e9330c8b14332e35ccd8
Andrey <shock.work@mail.ru>
2021-03-24 20:58:51 +0300

    show empty cart if no products

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import CartComponent from '../components/Cart/Cart';
 import EmptyCart from '../components/Cart/EmptyCart'
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
-    const flag = true;
+    const { items, totalCount, totalPrice } = useSelector(({ cart }) => cart);
+    const itemKeys = Object.keys(items);
+    const isEmpty = itemKeys.length;
 
     return (
         <div className="content">
             <div className="container container--cart">
-                {flag ? <CartComponent /> : <EmptyCart />}
+                {isEmpty ? (
+                    <CartComponent
+                        items={items}
+                        keys={itemKeys}
+                        totalCount={totalCount}
+                        totalPrice={totalPrice}
+                    />
+                ) : (
+                    <EmptyCart />
+                )}
             </div>
         </div>
     )
