d4c2957ba398d62fe98891c3c3063e1dae380a1d
Andrey <shock.work@mail.ru>
2021-03-24 20:59:10 +0300

    action for minus product

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -2,3 +2,8 @@ export const addPizzaToCart = (pizzaObj) => ({
     type: 'ADD_PIZZA_TO_CART',
     payload: pizzaObj
 });
+
+export const minusPizza = (key) => ({
+    type: 'MINUS_PIZZA',
+    payload: key
+});
