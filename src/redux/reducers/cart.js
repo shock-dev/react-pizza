2b9d57fe72995f2183aee145b5d4c897bd918e84
Andrey <shock.work@mail.ru>
2021-03-24 20:57:48 +0300

    reducer for minus product in cart

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -10,15 +10,44 @@ const cart = (state = initialState, action) => {
             const newItems = {
                 ...state.items,
                 [action.payload.id]: !state.items[action.payload.id]
-                    ? [action.payload]
-                    : [...state.items[action.payload.id], action.payload]
+                    ? {
+                        localTotalPrice: action.payload.price,
+                        pizzas: [action.payload]
+                    }
+                    : {
+                        localTotalPrice: state.items[action.payload.id].localTotalPrice + action.payload.price,
+                        pizzas: [...state.items[action.payload.id].pizzas, action.payload]
+                    }
             };
+
             return {
                 ...state,
                 items: newItems,
                 totalPrice: state.totalPrice + action.payload.price,
                 totalCount: ++state.totalCount
             };
+        case 'MINUS_PIZZA':
+            const pizzas = state.items[action.payload].pizzas;
+
+            if (pizzas.length > 1) {
+                const deletedPizza = pizzas.splice(pizzas.length - 1, 1)[0];
+
+                return {
+                    ...state,
+                    totalPrice: state.totalPrice - deletedPizza.price,
+                    totalCount: state.totalCount - 1,
+                    items: {
+                        ...state.items,
+                        [action.payload]: {
+                            ...state.items[action.payload],
+                            localTotalPrice: state.items[action.payload].localTotalPrice - deletedPizza.price,
+                            pizzas
+                        }
+                    }
+                }
+            }
+
+            return state;
         default:
             return state;
     }
