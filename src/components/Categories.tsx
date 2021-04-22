4be596d3826afde34159139c7b40c5a4828ccf42
Andrey <shock.work@mail.ru>
2021-04-22 15:45:13 +0300

    categories functional realized

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import classNames from 'classnames';
+import { CategoryType } from '../store/filters/types';
 
-const Categories = () => {
+interface CategoriesProps {
+  items: string[]
+  active: CategoryType
+  clickItemHandler: (val: CategoryType) => void
+}
+
+const Categories = ({ items, active, clickItemHandler }: CategoriesProps) => {
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          className={classNames({ active: active === null })}
+          onClick={clickItemHandler.bind(null, null)}
+        >
+          Все
+        </li>
+        {items.map((category, index) =>
+          <li
+            key={index}
+            className={classNames({ active: active === index })}
+            onClick={clickItemHandler.bind(null, index)}
+          >
+            {category}
+          </li>
+        )}
       </ul>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,31 @@ import React from 'react';
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import Card from '../components/Card';
+import categories from '../resources/categories';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCategory } from '../store/filters/selectors';
+import { CategoryType } from '../store/filters/types';
+import { setCategory } from '../store/filters/actions';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const activeCategory = useSelector(selectCategory);
+
+  const clickCategoryHandler = (val: CategoryType): void => {
+    if (val !== activeCategory) {
+      dispatch(setCategory(val));
+    }
+  };
+
   return (
     <div className="content">
       <div className="container">
         <div className="content__top">
-          <Categories />
+          <Categories
+            items={categories}
+            active={activeCategory}
+            clickItemHandler={clickCategoryHandler}
+          />
           <Sort />
         </div>
         <h2 className="content__title">Все пиццы</h2>
