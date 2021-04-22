e7754d25cc3e482dc64613dd37c39f7b57fe117b
Andrey <shock.work@mail.ru>
2021-04-22 21:11:51 +0300

    selectors for status of pizzas

--- src/store/pizzas/selectors.ts
+++ src/store/pizzas/selectors.ts
@@ -1,7 +1,11 @@
-import { RootState } from '../types';
+import { EntityStatus, RootState } from '../types';
 
 export const selectPizzasState = (state: RootState) => state.pizzas;
 
 export const selectPizzas = (state: RootState) => selectPizzasState(state).items;
 
 export const selectPizzasStatus = (state: RootState) => selectPizzasState(state).status;
+
+export const selectPizzasLoading = (state: RootState): boolean => selectPizzasStatus(state) === EntityStatus.LOADING;
+
+export const selectPizzasSuccess = (state: RootState): boolean => selectPizzasStatus(state) === EntityStatus.SUCCESS;
