4c72a1370c8673b6fdb1e60c291c665c9421c48d
Andrey <shock.work@mail.ru>
2021-04-25 16:13:25 +0300

    change total price and count in reducer

--- src/store/cart/reducer.ts
+++ src/store/cart/reducer.ts
@@ -11,12 +11,16 @@ const cart = produce((draft: Draft<CartState>, action: CartAction) => {
   switch (action.type) {
     case CartActionType.SET_CART_ITEM:
       draft.items.push(action.payload);
+      draft.totalPrice += action.payload.price;
+      draft.totalCount += 1;
       break;
 
     case CartActionType.PLUS_CART_ITEM:
       const index = draft.items.findIndex((item) => item.id === action.payload);
       // @ts-ignore
       ++draft.items[index].count;
+      draft.totalPrice += draft.items[index].price;
+      draft.totalCount += 1;
       break;
     default:
       break;
