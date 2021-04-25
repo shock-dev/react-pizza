303efa6076c313fd68de647dc739225b75623c11
Andrey <shock.work@mail.ru>
2021-04-25 18:07:25 +0300

    action for minus

--- src/store/cart/actions.ts
+++ src/store/cart/actions.ts
@@ -1,4 +1,4 @@
-import { CartActionType, ICartItem, PlusCartItemAction, SetCartItemAction } from './types';
+import { CartActionType, ICartItem, MinusCartItemAction, PlusCartItemAction, SetCartItemAction } from './types';
 
 export const setCartItem = (payload: ICartItem): SetCartItemAction => ({
   type: CartActionType.SET_CART_ITEM,
@@ -9,3 +9,9 @@ export const plusCartItem = (payload: ICartItem['id']): PlusCartItemAction => ({
   type: CartActionType.PLUS_CART_ITEM,
   payload
 });
+
+export const minusCartItem = (payload: ICartItem['id']): MinusCartItemAction => ({
+  type: CartActionType.MINUS_CART_ITEM,
+  payload
+});
+
